e37987b4f88e13a47aaea5d6ec061d8e01edcd6f
Ilya Zinkovich <ilyazinkovich@Ilyas-MacBook-Pro.local>
2016-08-28 23:22:48 +0300

    Modified students room view

--- campus-ui/app/assets/parts/content/students/students.controller.js
+++ campus-ui/app/assets/parts/content/students/students.controller.js
@@ -1,6 +1,14 @@
 angular.module("campus").controller("StudentsCtrl", ["$scope", "RoomService", function ($scope, RoomService) {
 
-    $scope.rooms = RoomService.getAll();
+    RoomService.getAll().then(function(response) {
+        $scope.rooms = response.data;
+        console.log($scope.rooms);
+    }, function(data) {
+        console.log("error");
+    });
 
-
+    $scope.setProfileToShow = function(profile) {
+        $scope.profileToShow = profile;
+        console.log(profile);
+    };
 }]);
--- campus-ui/app/assets/parts/content/students/students.js
+++ campus-ui/app/assets/parts/content/students/students.js
@@ -8,4 +8,15 @@ angular.module("campus").config(["$stateProvider", function ($stateProvider) {
           }
       }
   });
-}]);
+}])
+.filter('ageFilter', function() {
+     function calculateAge(birthday) {
+         var ageDifMs = Date.now() - birthday.getTime();
+         var ageDate = new Date(ageDifMs);
+         return Math.abs(ageDate.getUTCFullYear() - 1970);
+     }
+
+     return function(birthdate) {
+           return calculateAge(new Date(birthdate));
+     };
+});;
