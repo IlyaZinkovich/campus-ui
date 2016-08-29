1eefff3ea0f8261c29acf7145bf17f7afd4510ed
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2016-08-29 21:37:56 +0300

    Added logout

--- campus-ui/app/assets/parts/content/events/events.js
+++ campus-ui/app/assets/parts/content/events/events.js
@@ -1,11 +1,11 @@
-angular.module("campus").config(["$stateProvider", function ($stateProvider) {
-  $stateProvider.state("init.events", {
-      url : "/events",
-      views: {
-          "content@": {
-              templateUrl: "assets/parts/content/events/events.html",
-              controller: "EventsCtrl"
-          }
-      }
-  });
+angular.module("campus").config(["$stateProvider", function($stateProvider) {
+    $stateProvider.state("init.events", {
+        url: "/events",
+        views: {
+            "content@": {
+                templateUrl: "assets/parts/content/events/events.html",
+                controller: "EventsCtrl"
+            }
+        }
+    });
 }]);
--- campus-ui/app/assets/parts/header/header.controller.js
+++ campus-ui/app/assets/parts/header/header.controller.js
@@ -3,21 +3,9 @@ angular.module("campus").controller("HeaderCtrl", ["$scope", "$rootScope", "$sta
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
     });
-  // $scope.isAuthorized = localStorageService.cookie.get("isAuthorized");
-  // $scope.user = localStorageService.cookie.get("user");
-  //
-  // $scope.logout = function () {
-  //   localStorageService.cookie.set("isAuthorized", false);
-  //   $state.reload();
-  // };
-  //
-  // var campus = $("#campus");
-  //
-  // if (!$scope.isAuthorized) {
-  //   campus.removeClass("col-lg-offset-2");
-  //   campus.addClass("col-lg-offset-4");
-  // } else {
-  //   campus.addClass("col-lg-offset-2");
-  //   campus.removeClass("col-lg-offset-4");
-  // }
+
+    $scope.logout = function() {
+        $rootScope.isAuthenticated = false;
+        $state.go("login");
+    };
 }]);
